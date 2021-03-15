15a57d8374e95e6165751cee04040c8dc1b857d0
Dmitry Burin <dmitry_burin@epam.com>
2021-03-15 13:51:41 +0300

    Update encode file

--- api/encode/encode.go
+++ api/encode/encode.go
@@ -29,3 +29,14 @@ func Encode(str string) bytes.Buffer {
 
 	return buff
 }
+
+// Decoder return decoded struct
+func Decode(buff bytes.Buffer) TextMsg {
+	msg := TextMsg{}
+
+	fmt.Printf("Decode msg\n")
+	dec := gob.NewDecoder(&buff)
+	dec.Decode(&msg)
+	fmt.Printf("Decoded msg: (err=%v) - %v\n", err, msg)
+	return msg
+}
